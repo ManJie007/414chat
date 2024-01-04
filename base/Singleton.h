c5eb22ddd5420498de04f8976d9f4753e2d982d4
林顺涛 <2521553670@qq.com>
2024-01-04 15:21:39 +0800

    修复了SingleTon.h中多线程同时访问instance时出现的竞态

--- base/Singleton.h
+++ base/Singleton.h
@@ -1,26 +1,26 @@
 #pragma once
-
+#include<pthread.h>
 template<typename T>
 class Singleton
 {
 public:
 	static T& Instance()
 	{
-		//pthread_once(&ponce_, &Singleton::init);
-		if (nullptr == value_)
-		{
-			value_ = new T();
-		}
+		pthread_once(&ponce_, &Singleton::init);
+		// if (nullptr == value_)
+		// {
+		// 	value_ = new T();
+		// }
 		return *value_;
 	}
-
+	Singleton()=delete;
 private:
-	Singleton();
 	~Singleton() = default;
 
 	Singleton(const Singleton&) = delete;
 	Singleton& operator=(const Singleton&) = delete;
-
+	//要写成静态方法的原因：类成员函数隐含传递this指针（第一个参数）
+	//而pthread_once函数标签为int pthread_once(pthread_once_t once_control, void (init_routine) (void))；
 	static void init()
 	{
 		value_ = new T();
@@ -31,9 +31,7 @@ private:
 	{
 		delete value_;
 	}
-
-private:
-	//static pthread_once_t ponce_;
+	static pthread_once_t ponce_;
 	static T*             value_;
 };
 
